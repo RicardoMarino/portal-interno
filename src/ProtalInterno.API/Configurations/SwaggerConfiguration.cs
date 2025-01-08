a8ad896728d1631214ed6e2d5e8c540fc8dd1db1
Ricardo Marino <ricardomarino@live.com>
2025-01-08 17:28:22 -0300

    Criado arquivo de configuração do Swagger, para isolar no código.

---
+++ src/ProtalInterno.API/Configurations/SwaggerConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace ProtalInterno.API.Configurations;
+
+public static class SwaggerConfiguration
+{
+    public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProtalInterno.API", Version = "v1" });
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Por favor, insira 'Bearer' [espaço] e o seu token JWT",
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+            
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    []
+                }
+            });
+        });
+        return services;
+    }
+}
--- src/ProtalInterno.API/Program.cs
+++ src/ProtalInterno.API/Program.cs
@@ -1,4 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "V1"));
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
